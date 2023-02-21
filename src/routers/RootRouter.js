3b21784143abd61acdeda6b43bc111b7ca0eda79
QuanDinh1211 <dinhvanquan1211@gmail.com>
2023-02-21 17:10:39 +0700

    product detail screen lap

--- src/routers/RootRouter.js
+++ src/routers/RootRouter.js
@@ -3,13 +3,19 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "../screen/Home";
 import ProductDetail from "../screen/ProductDetail";
+import ProductDetailTow from "../screen/ProductDetail/ProductDetailTow";
 
 const RootRouter = () => {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/:category/:producid" element={<ProductDetail />} />
+        <Route
+          exact
+          path="/:category/:producid"
+          element={<ProductDetailTow />}
+        />
+        {/* <Route exact path="/:category/:producid" element={<ProductDetail />} /> */}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </>
---
+++ src/screen/Cart/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../../assets/styles/cartStyle.scss";
+
+const Cart = () => {
+  return <div>cart</div>;
+};
+
+export default Cart;
